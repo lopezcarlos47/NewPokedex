250920507782e2cf2d0c3b57dd036616f81e2090
lopezcarlops47 <carlosignacio300lopezgmail.com>
2019-06-01 16:32:34 -0600

    poke

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // Write your code here
+var Pokemon = angular.module("Pokemon",[])
+Pokemon.controller("ListadoPokemon",function($scope,$rootScope,$http){
+	$scope.Lista = [];
+	for(var y=1;y<=100;y++){
+		$http({
+		method:"GET",
+		url:"https://pokeapi.co/api/v2/pokemon/"+y
+	}).then(function datos(x){
+		$scope.Lista.push(x);
+	})
+	}
+	
+});
